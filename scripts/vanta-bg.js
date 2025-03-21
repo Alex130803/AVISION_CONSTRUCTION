544b16d550d90eff4d276070e70ec5211936ebbc
Alex130803 <ai3522@g.rit.edu>
2025-03-22 00:39:50 +0100

    UPG

--- scripts/vanta-bg.js
+++ scripts/vanta-bg.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const isMobile = window.innerWidth < 768;
+
     VANTA.NET({
         el: "#vanta-bg",
         color: 0x4474BA,
         backgroundColor: 0xF5F5F5,
-        points: window.innerWidth < 768 ? 15 : 12,           // More points on mobile
-        maxDistance: window.innerWidth < 768 ? 25 : 20,      // Bring lines closer on mobile
-        spacing: window.innerWidth < 768 ? 18 : 20,          // Slightly tighter net on mobile
+        points: 14,
+        maxDistance: 20,
+        spacing: isMobile ? 16 : 20,     // Tighter spacing on phone
         showDots: true,
         mouseControls: true,
         touchControls: true,
